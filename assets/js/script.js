ffe177c771b19b46a91865fb658953c00df570ba
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-25 14:15:31 -0500

    Completed guessing and added ui elements to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
+startBtn = document.querySelector("#startBtn");
+wordDisplay = document.querySelector("#wordDisplay");
+
 wordsToChooseFrom = ["javascript", "string", "object", "method", "null", "undefined"];
 chosenWord = '';
 chosenWordArr = []
 guessedWord = '';
 
+function winScenario(){
+    wordDisplay.textContent = "You Win!";
+}
+
+function fillPTag(){
+    wordDisplay.textContent = guessedWord;
+}
+
 function checkKeyAgainstWord(event) {
     //if event.key matches any character in our chosen word
-    console.log(event.key)
     if (chosenWord.includes(event.key.toLowerCase()) && !(event.key == " ")) {
-        console.log("guess correctly")
         chosenWordArr.forEach((char, i) => {
             if (char == event.key.toLowerCase()){
-                console.log("character match", char, i)
-                guessedWord[i] = char;
-                console.log(guessedWord);
+                var firstPartOfWord = guessedWord.substr(0, i);
+                var secondPartOfWord = guessedWord.substr(i + 1, guessedWord.length);
+                guessedWord = firstPartOfWord + char + secondPartOfWord;
+                console.log(guessedWord)
             }
         });
+
+        fillPTag();
+
+        if (!guessedWord.includes("_")){
+            winScenario();
+        }
     }
 }
 
@@ -31,19 +47,22 @@ function chooseRandomWord() {
     console.log(chosenWordArr)
     console.log(chosenWord)
     console.log(guessedWord);
+
+    fillPTag();
 }
 
 document.addEventListener("keydown", checkKeyAgainstWord);
 
-chooseRandomWord();
+startBtn.addEventListener("click", chooseRandomWord);
 
 /* 
 1. ✅ keydown working
 2. ✅ array of a couple of words to choose from
 3. ✅ see if that key thats pressed matches the word that was chosen
 4. ✅ second string for whats guessed
-5. fill guessed word with underscores
-6. when they press a key if it matches add the correct character in the place of the underscore
-7. 
-8.
+5. ✅ fill guessed word with underscores
+6. ✅ when they press a key if it matches add the correct character in the place of the underscore
+7. button, display blanks,
+7. build a timer, display timer
+8. display win
  */
